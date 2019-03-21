bcc047e66773c039dfeeb51cecc615c21db15fd2
Shion <shionguha@gmail.com>
2019-03-21 17:52:14 -0500

    updated datasets

--- scripts/3-way-anova.R
+++ scripts/3-way-anova.R
@@ -1,12 +1,13 @@
 url: https://github.com/quantide/qdata
 
+#setup
 head(distance)
-
 str(distance)
 
+#summary plot
 plot.design(Distance ~ ., data = distance)
 
-	
+#2-way interaction plot	
 op <- par(mfrow = c(1, 1))
 with(distance, {
   interaction.plot(ABS, Tire, Distance)
@@ -16,21 +17,26 @@ with(distance, {
 )
 par(op)
 
+#doing the anova
 fm <- aov(Distance ~ ABS * Tire * Tread, data = distance)
 summary(fm)
 
+#remove 3 way interaction
 fm <- update(fm, . ~ . -ABS:Tire:Tread)
 summary(fm)
 
+#remove 2 way interaction
 fm1 <- update(fm, .~ABS+Tire+Tread)
 summary(fm1)
 
+#comparing full and reduced model
 anova(fm, fm1)
 
+#table of means from model
 model.tables(fm1,type="effects")
-
 model.tables(fm1,type="means")
 
+#final model with significant effects
 fm <- aov(Distance ~ ABS + Tire, data = distance)
 summary(fm)
 
